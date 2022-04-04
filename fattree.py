65ce1252fda72e06553fdbcbdfba94e99cadacc0
jaedusg <44039856+jaedusg@users.noreply.github.com>
2022-04-04 01:57:26 -0400

    Add files via upload

--- fattree.py
+++ fattree.py
@@ -1,162 +1,165 @@
-import sys, time
 from mininet.topo import Topo
-from mininet.link import TCLink
 from mininet.net import Mininet
-from mininet.util import waitListening
-from mininet.node import Controller, RemoteController
 from mininet.node import Node
+from mininet.node import Controller, RemoteController
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.util import waitListening
+import sys, time
 
 
 class FatTree( Topo ):
 
-    CoreSwitches = []
-    AggSwitches = []
-    EdgeSwitches = []
-    Hosts = []
+    CoreSwitchList = []
+    AggSwitchList = []
+    EdgeSwitchList = []
+    HostList = []
 
-    """
-    Initialize Topology 
-    """
-    
     def __init__( self, k):
+        " Create Fat Tree topo."
         self.pod = k
-        self.iCoreLayerSwitch = pow((k/2),2)
-        self.iAggLayerSwitch = ((k/2)*k)
-        self.iEdgeLayerSwitch = ((k/2)*k)
-        self.iHost = (k*pow((k/2),2))
+        self.iCoreLayerSwitch = (k/2)**2
+        self.iAggLayerSwitch = k*k/2
+        self.iEdgeLayerSwitch = k*k/2
         self.density = k/2
+        self.iHost = self.iEdgeLayerSwitch * self.density
 
-        # Initialize Topology
+
+
+        # Init Topo
         Topo.__init__(self)
-        # Create Switches Core, Aggregation, Edges
-        print("\n Begin Creating Virtual Network \n")
-        self.createSwitch(self.iCoreLayerSwitch, "cs_", self.CoreSwitches)
-        self.createSwitch(self.iAggLayerSwitch, "as_", self.AggSwitches)
-        self.createSwitch(self.iEdgeLayerSwitch, "es_", self.EdgeSwitches)
+
+        self.createTopo()
+
+        self.createLink(1000)
+
+    def createTopo(self):
+        self.createCoreLayerSwitch(self.iCoreLayerSwitch)
+        self.createAggLayerSwitch(self.iAggLayerSwitch)
+        self.createEdgeLayerSwitch(self.iEdgeLayerSwitch)
         self.createHost(self.iHost)
-        print("\n End Creating Virtual Network \n")
-        # Create Network Links with bandwidth 1000 MB
-        self.createNetworkLink(1000)
 
     """
-    Create Switch 
+    Create Switch and Host
     """
 
-    def createSwitch(self, numSW, prefix, switchList):
-        print("\n Begin Creating Switches " + prefix)
-        for s in range(1, numSW + 1):
-            switchList.append(self.addSwitch(prefix + str(s)))
-            print('Creating Switch: ' + prefix + str(s))
-            
-        print("\n End Creating Switches " + prefix) 
-    
-        
-    """
-    Create Host
-    """
-    
-    def createHost(self, numHost):
-        print("\n Begin Creating Hosts \n")
-        for h in range(1, numHost + 1):
-            prefix = "h_"
-            self.Hosts.append(self.addHost(prefix + str(h)))
-            print('Creating Host: ' + prefix + str(h))
-            
-        print("\n End Creating Hosts \n")    
-    
+    def _addSwitch(self, number, level, switch_list):
+        for x in xrange(1, number+1):
+            PREFIX = str(level) + "00"
+            if x >= int(10):
+                PREFIX = str(level) + "0"
+            switch_list.append(self.addSwitch('s' + PREFIX + str(x)))
+
+    def createCoreLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 1, self.CoreSwitchList)
+
+    def createAggLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 2, self.AggSwitchList)
+
+    def createEdgeLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 3, self.EdgeSwitchList)
+
+    def createHost(self, NUMBER):
+        for x in xrange(1, NUMBER+1):
+            PREFIX = "h"
+            self.HostList.append(self.addHost(PREFIX + str(x)))
 
     """
-    Create Network Link
+    Add Link
     """
-    def createNetworkLink(self, bandwidth):
-        # bandwidth in Megabytes
-        linkbw = dict(bw=bandwidth) 
+    def createLink(self, bandwidth):
+        # bandwidth in Mbit
+        linkopts = dict(bw=bandwidth) 
         end = self.pod/2
-        for x in range(0, self.iEdgeLayerSwitch):
-            for y in range(0,end):
-                self.addLink(self.EdgeSwitches[x], self.Hosts[end * x + y], **linkbw)
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.CoreSwitchList[i*end+j],
+                        self.AggSwitchList[x+i],
+                        **linkopts)
 
-        for x in range(0, self.iAggLayerSwitch):
-            for y in range(0,end):
-                self.addLink(self.AggSwitches[x], self.EdgeSwitches[end * (x/end) + y], **linkbw)
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.AggSwitchList[x+i],
+                        self.EdgeSwitchList[x+j],
+                        **linkopts)
 
-        for x in range(0, self.iAggLayerSwitch, end):
-            for y in range(0,end):
-                for z in range(0,end):
-                    self.addLink(self.CoreSwitches[y * end + z], self.AggSwitches[x + y], **linkbw)
+        for x in xrange(0, self.iEdgeLayerSwitch):
+            for i in xrange(0, self.density):
+                self.addLink(
+                    self.EdgeSwitchList[x],
+                    self.HostList[self.density * x + i],
+                    **linkopts)
 
+"""
+Set IP
+"""
+def setHostIp(net, topo):
+    hosts = []
+    for k in xrange(len(topo.HostList)):
+        hosts.append(net.get(topo.HostList[k]))
 
-    """
-    Assign IPs for Hosts
-    """
-    def assignIp(network, topology):
-        hosts = []
-        ipSegmentFormat = "10.%d.%d.%d"
-        i = 0
-        end = topology.pod/2
-        
-        for x in range(len(topology.Hosts)):
-            hosts.append(network.get(topology.Hosts[x]))
-        
-        for pod in range(1, topology.pod + 1):
-            for edgeSwNum in range(1, end+1):
-                for hostNumInSw in range(1, end+1):
-                    print("pod = " + str(pod) + " / edgeSwitchNumber = " + str(edgeSwNum) + " / hostNumberInSwitch = " + str(hostNumInSw))
-                    hosts[i].setIP(ipSegmentFormat % (pod, edgeSwNum, hostNumInSw))
-                    print(hosts[i].IP())
-                    i += 1
+    h = 0
+    end = topo.pod/2
+    print (end)
+    for pod in range(1, topo.pod + 1):
+        for edgeSwitchNummber in range(1, end+1):
+            for hostNbInSwitch in range(1, end+1):
+                print("pod = " + str(pod) + " / edgeSwitchNummber = " + str(edgeSwitchNummber) + " / hostNbInSwitch = " + str(hostNbInSwitch))
+                hosts[h].setIP("10.%d.%d.%d" % (pod, edgeSwitchNummber, hostNbInSwitch))
+                print(hosts[h].IP())
+                print(h+1)
+                h += 1
                 
 
 
-    topos = { 'fattree' : ( lambda k : FatTree(k)) }
+topos = { 'fattree' : ( lambda k : FatTree(k)) }
 
-    def createTraffic(network, topology):
-        print("\n Creating Traffic \n")
-        srvCommand = "iperf -s -u &"
-        clCmd1 = "iperf -c "
-        clCmd2 = " -u -b "
-        clCmd3 = "m -t 120 &"
-        for bw in range(100, 1000, 100):
-            print('bandwidth: ' + bw)
-            for i in range(topology.iHost//2):
-                destination = network.hosts[2 * i + 1]
-                # Create the server command and execute it
-                destination.cmd(srvCommand)
-                source = network.hosts[i]
-                print("IP Source: " + str(source.IP()) + " >> " + " IP Destination: " + str(destination.IP()))
-                # Create the client command and execute it
-                clCmd = clCmd1 + str(destination.IP()) + clCmd2 + str(bw) + clCmd3
-                source.cmd(clCmd)
-            time.sleep(120)
 
-    def runSimulation(k, traffic):
-        "Create Fat Tree network"
-        mn_topo = FatTree(k)
-        # Create a remote controller object in local host
-        RemoteCon = RemoteController("c0", ip="127.0.0.1")
-        network = Mininet(topo=mn_topo, link = TCLink, controller=RemoteCon) 
-        network.start()
-        # Assign IP addresses.
-        assignIp(network, mn_topo)
-        print("k: " + str(k) + " , " + "traffic: " + traffic)
-        if (traffic == "traffic"):
-            # Ping all
-            network.pingAll()
-            time.sleep(15)
-            # Traffic generation
-            createTraffic(network, mn_topo)
-        else:
-            # CLI running
-            CLI(network)
-        network.stop()
-        
+def runMyNetwork(k, traffic):
+    "Create Fat Tree network"
+    mytopo = FatTree(k)
+    net = Mininet(topo=mytopo, link = TCLink, controller=RemoteController( 'c0', ip='127.0.0.1')) #TODO : mettre le bon controleur
+    net.start()
+    # Set hosts IP addresses.
+    setHostIp(net, mytopo)
+    print(traffic)
+    if (traffic == "traffic"):
+        # Ping all
+        net.pingAll()
+        time.sleep(10)
+        # Traffic generation
+        generateTraffic(net,mytopo)
+    else:
+        # CLI running
+        CLI(net)
+    net.stop()
     
+def generateTraffic(net, topo):
+# Uniform traffic generator
+    print("\n TRAFFIC GENERATOR \n")
+    for bandwidth in range(100,1000, 100):
+        print(bandwidth)
+        for h in range(topo.iHost//2):
+            dst = net.hosts[2*h +1]
+            # Create the server command and sends it
+            serverCmd = "iperf -s -u &"
+            dst.cmd(serverCmd)
+            src = net.hosts[h]
+            print(str(src.IP()) + " >> " + str(dst.IP()))
+            # Create the client command and sends it
+            clientCmd = "iperf -c " + str(dst.IP()) + " -u -b " + str(bandwidth) + "m -t 120 &"
+            src.cmd(clientCmd)
+        time.sleep(120)
 
-    if __name__ == '__main__':
-        setLogLevel('info')
-        if (len(sys.argv) != 3):
-            print("your command must have following structure : sudo python fattreevnetwork.py k traffic/notraffic")
-        else: 
-            runSimulation(int(sys.argv[1]), sys.argv[2])
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    if (len(sys.argv) != 3):
+        print("Usage : sudo python fattree.py k traffic/notraffic")
+    else: 
+        runMyNetwork(int(sys.argv[1]), sys.argv[2])
